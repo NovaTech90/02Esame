568e69d28e010810dca1bc5d6248110f15b4e436
Valentina <nova.tech.319@gmail.com>
2024-09-25 22:15:42 +0200

    Sistemato il collegamento tra la pagina portfolio e la pagina di dettaglio del lavoro

--- work.php
+++ work.php
@@ -11,23 +11,30 @@ include('./modules/head.php');
 
     <section id="work">
         <div class="container container-work">
-            <?php
-            $json_data = file_get_contents("./files/works.json");
-            $works = json_decode($json_data, true);
-            if (count($works) != 0) {
-                foreach ($works as $work) {
-            ?>
-                    <img src="./img/card.jpg" alt="" class="work-img">
-                    <div class="work-text">
-                        <h1><?php echo $work['title'] ?></h1>
-                        <h5><?php echo $work['subtitle'] ?></h5>
-                        <p><?php echo $work['description'] ?></p>
-                        <button type="button" class="btn"><a href="">Let's Talk</a></button>
-                    </div>
-            <?php
-                }
-            }
-            ?>
+        <?php
+$json_data = file_get_contents("./files/works.json");
+$works = json_decode($json_data, true);
+if ($works) {
+    foreach ($works as $work) {
+        if ($work['id'] ==$_GET['id']) {
+?>
+            <img src="./img/card.jpg" alt="" class="work-img">
+            <div class="work-text">
+                <h1><?php echo $work['title'] ?></h1>
+                <h5><?php echo $work['subtitle'] ?></h5>
+                <p><?php echo $work['description'] ?></p>
+                <button type="button" class="btn"><a href="">Let's Talk</a></button>
+            </div>
+<?php
+        }
+    }
+}
+?>
+
+
+
+
+
         </div>
     </section>
     <?php
